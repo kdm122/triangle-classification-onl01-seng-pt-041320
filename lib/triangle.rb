9b66710bedecb823c72c3c104c102586665fc582
Kyle McLean <kyledmclean@gmail.com>
2020-05-24 19:10:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+  end
+
+#  def self.error
+#    begin
+#        raise TriangleError
+#      rescue TriangleError => error
+#        puts error.message
+#    end
+#  end
+
+  def kind
+    if @sides.any? { |side| side == 0 || side < 0 }
+      raise TriangleError
+    elsif (@side1+@side2 <= @side3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
+      raise TriangleError
+    else
+      if (@side1 == @side2) && (@side2 == @side3)
+        :equilateral
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        :isosceles
+      elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
+        :scalene
+      end
+    end
+
+
+
+  end
+
+  class TriangleError < StandardError
+    def message
+    "Not a valid triangle"
+    end
+  end
+
+
 end
